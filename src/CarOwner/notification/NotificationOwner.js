096f57ad022b8a0279953e17b8e7f66f8e7bc6cc
imac <imac@iMac.local>
2025-11-11 11:16:53 +0530

    working on the notification folder

---
+++ src/CarOwner/notification/NotificationOwner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+
+const NotificationOwner = () => {
+    return (
+        <View>
+            <Text>
+                c kdgfcdf
+            </Text>
+        </View>
+    )
+}
+
+export default NotificationOwner;
